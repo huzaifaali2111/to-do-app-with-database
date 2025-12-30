555a604aab1db3597e2e0eda90caf4f3f5155c26
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2025-12-30 18:39:43 +0500

    feat: implement dynamic task rendering and enhance error handling in todo app

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-// const dotenv = require("dotenv");
-// const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const path = require('path');
 const mongoose = require('mongoose');
 const Todo = require("./models/todo");
-// dotenv.config();
-// connectDB();
+dotenv.config();
+connectDB();
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -13,13 +13,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 
-app.get("/", (req, res) => {
-  res.render("index")
+app.get("/", async (req, res) => {
+  try {
+    const allTasks = await Todo.find();
+    res.render("index", { todos: allTasks });
+  } catch (err) {
+    console.error("Error fetching tasks:", err);
+    res.status(500).send("Server Error");
+  }
 });
+
 app.post("/add-todo", async (req, res) => {
   try {
     const { task, priority, dueDate } = req.body;
-    if (!task|| task.trim() === "") {
+    if (!task || task.trim() === "") {
       return res.status(400).send("Task cannot be empty!");
     }
     const newTodo = new Todo({
@@ -36,7 +43,10 @@ app.post("/add-todo", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
-
+app.post("/delete-todo/:id", async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
 
 app.listen(5000, () => {
   console.log("Server started on port 5000");
