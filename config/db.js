d1821c8300d6e30f61ae38103ed2dc727c7a7f49
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2025-12-31 15:16:35 +0500

    fix: update database connection URI to use DB_URI environment variable

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI_local);
+    await mongoose.connect(process.env.DB_URI);
     console.log("Database Connected Successfully");
   } catch (error) {
     console.error("Database Connection Failed", error.message);
